4ab8ef4810e37114268159f80b96b670021977d6
Spencer Heywood <l.spencer.heywood@protonmail.com>
2023-06-15 11:15:06 -0600

    removed code-server, added my nix template
    Signed-off-by: Spencer Heywood <l.spencer.heywood@protonmail.com>

--- nix/main.tf
+++ nix/main.tf
@@ -31,7 +31,8 @@ resource "coder_agent" "main" {
   startup_script         = <<-EOT
     set -e
 
-    # start code-server
+    # install and start code-server
+    curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server --version 4.11.0
     /tmp/code-server/bin/code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
   EOT
 
